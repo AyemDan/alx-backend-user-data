f130c10d64c39711c6aae82336161129f8114991
AyemDanm <danielMMomodu@gmail.com>
2024-11-12 12:53:01 +0100

    Corrected pycodestyle

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -3,13 +3,13 @@
 from typing import List, TypeVar
 from flask import request, jsonify, make_response
 
+
 class Auth:
-    
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         return False
-    
+
     def authorization_header(self, request=None) -> str:
         return None
-    
+
     def current_user(self, request=None) -> TypeVar('User'):
         return None
