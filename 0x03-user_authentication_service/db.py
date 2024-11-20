f0983e3d1aa3039aa6213612df727a83bd511161
AyemDanm <danielMMomodu@gmail.com>
2024-11-20 11:17:43 +0100

    fix

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,19 +1,51 @@
 """DB module
+This module contains the `DB` class responsible for
+interacting with the database.
+It uses SQLAlchemy to handle database operations,
+including user creation and session management.
 """
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-
 from user import Base, User
 
 
 class DB:
-    """DB class
+    """
+    DB class
+
+    The `DB` class handles database operations, including
+    establishing a connection
+    to the database and interacting with the `users` table.
+    It uses SQLAlchemy ORM
+    to add new users and manage database sessions.
+
+    Attributes:
+    -----------
+    _engine : sqlalchemy.engine.Engine
+        The engine used to connect to the SQLite database.
+
+    __session : sqlalchemy.orm.Session
+        The session object used to interact with the database.
+
+    Methods:
+    --------
+    _session : sqlalchemy.orm.Session
+        A memoized session object to interact with the database.
+
+    add_user(email: str, hashed_password: str) -> User
+        Adds a new user to the database with the given
+        email and hashed password.
     """
 
     def __init__(self) -> None:
         """Initialize a new DB instance
+
+        Initializes the SQLite engine, creates the database schema
+        (or drops it and
+        recreates it), and sets up a session.
         """
         self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
@@ -23,6 +55,15 @@ class DB:
     @property
     def _session(self) -> Session:
         """Memoized session object
+
+        Returns a session object that is used for interacting
+        with the database.
+        If the session doesn't exist, it is created and stored.
+
+        Returns:
+        --------
+        sqlalchemy.orm.Session
+            The session object to interact with the database.
         """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
@@ -31,6 +72,24 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """Add a new user to the database
+
+        Creates a new user record in the database with the given
+        email and hashed password.
+
+        Parameters:
+        -----------
+        email : str
+            The email of the user to be added.
+
+        hashed_password : str
+            The hashed password of the user to be added.
+
+        Returns:
+        --------
+        User
+            The newly created user object.
+
+        Closes the session after committing the transaction.
         """
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
